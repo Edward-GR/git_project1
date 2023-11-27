15906799e8d41efe2b657793d2f0a234782e2c31
Edward-GR <151847175+Edward-GR@users.noreply.github.com>
2023-11-27 13:13:44 +0300

    Update program.py
    new line was added

--- program.py
+++ program.py
@@ -2,6 +2,6 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('3 raz')
-
+    print('UFO came and added this line')
 if __name__ == '__main__':
     main()
